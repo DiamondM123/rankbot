4d489295bfa0ecb3b7b367fbe575be6a8a97643c
DiamondM123 <63083781+DiamondM123@users.noreply.github.com>
2020-04-05 12:44:02 -0500

    Update index.js

--- index.js
+++ index.js
@@ -86,92 +86,96 @@ const getRequest = async (mode, warid) => {
 
 client.on('message', async msg => {
 	try {
-		const commandParams = msg.content.split(" ")
-		const modes = commandParams[0].split("!")
-		const globalMode = modes[1]
-		//console.log(msg.guild.members)
-		//...
-		if (msg.content.startsWith("!rt") || msg.content.startsWith("!ct")) {
-			if (!msg.member.hasPermission("MANAGE_ROLES")) return msg.reply("This command is for legendary bosses of the almighty MMR system only")
-			const rtRoles = ["RT Bronze", "RT Silver I", "RT Silver II", "RT Gold I", "RT Gold II", "RT Platinum", "RT Diamond", "RT Master"]
-			const ctRoles = ["CT Bronze", "CT Silver I", "CT Silver II", "CT Gold", "CT Platinum", "CT Diamond", "CT Master"]
-			const modeRoles = (globalMode === "rt") ? rtRoles : ctRoles
-			const result = await getRequest(globalMode, commandParams[1])
-			if (!result) {
-				msg.author.send("Error. Unable to find player/event")
-				return msg.delete()
-			}
-			var mentionPlayers = ''
-
-
-			if (isNaN(commandParams[1])) {
-				//extra logic for additional players
-				let currentPlayer = msg.guild.members.cache.find(member => member.displayName.toLowerCase().replace(/\s/g, '').replace("é", '') === result[0].toLowerCase().replace(/\s/g, '').replace("é", ''))
-				if (currentPlayer === undefined) {
-					msg.author.send("Unable to find server member with the name " + commandParams[1])
-					return msg.delete()
-				}
-				for (j = 0; j < modeRoles.length; j++) {
-					currentPlayer = msg.guild.members.cache.find(member => member.displayName.toLowerCase().replace(/\s/g, '').replace("é", '') === result[0].toLowerCase().replace(/\s/g, '').replace("é", '') && member.roles.cache.some(role => role.name === modeRoles[j]))
-					if (currentPlayer !== undefined)
-						break
-				}
-				if (currentPlayer === undefined) {
-					msg.author.send(commandParams[1] + " does not have a rank role yet.")
-					return msg.delete()
-				}
-				//end
-				let serverRole = msg.guild.roles.cache.find(role => role.name === result[1])
-				if (!currentPlayer.roles.cache.some(role => role.name === serverRole.name)) {
-					for (j = 0; j < modeRoles.length; j++) {
-						if (currentPlayer.roles.cache.some(role => role.name === modeRoles[j]))
-							currentPlayer.roles.remove(currentPlayer.roles.cache.find(role => role.name.toLowerCase() === modeRoles[j].toLowerCase()))
-					}
-					let fromPenText = (commandParams[2] === "np") ? "" : "(from pen)"
-					currentPlayer.roles.add(serverRole.id)
-					mentionPlayers += `${currentPlayer} :` + result[1].replace(globalMode.toUpperCase() + " ", '').toLowerCase().replace(" ii", ': II').replace(" i", ': I').replace("platinum", "plat")
-					mentionPlayers += (mentionPlayers[mentionPlayers.length-1] === "I") ? ` ${fromPenText}\n` : `: ${fromPenText}\n`
-				}
-			} else {
-				if (result !== ',') {
-					let resultarray = result.split(",")
-					const ranks = resultarray.slice(0, resultarray.length/2)
-					const players = resultarray.slice(resultarray.length/2, resultarray.length)
-					for (i = 0; i < players.length; i++) {
-						let currentPlayer = msg.guild.members.cache.find(member => member.displayName.toLowerCase().replace(/\s/g, '').replace("é", '') === players[i].toLowerCase().replace(/\s/g, '').replace("é", ''))
-						if (currentPlayer === undefined) {
-							msg.author.send("Unable to find server member with the name " + players[i]).catch((e) => console.log(e))
-							continue
-						}
-						//...
-						for (j = 0; j < modeRoles.length; j++) {
-							currentPlayer = msg.guild.members.cache.find(member => member.displayName.toLowerCase().replace(/\s/g, '').replace("é", '') === players[i].toLowerCase().replace(/\s/g, '').replace("é", '') && member.roles.cache.some(role => role.name === modeRoles[j]))
-							if (currentPlayer !== undefined)
-								break
-						}
-						if (currentPlayer === undefined) {
-							msg.author.send(players[i] + " does not have a rank role yet.").catch((e) => console.log(e))
-							continue
-						}
-						//...
-						mentionPlayers += `${currentPlayer} :` + ranks[i].replace(globalMode.toUpperCase() + " ", '').toLowerCase().replace(" ii", ': II').replace(" i", ': I').replace("platinum", "plat")
-						mentionPlayers += (mentionPlayers[mentionPlayers.length-1] === "I") ? "\n" : ":\n"
-						let serverRole = msg.guild.roles.cache.find(role => role.name === ranks[i])
-						const specialRole = modeRoles[modeRoles.indexOf(ranks[i])]
-						for (j = 0; j < modeRoles.length; j++) {
-							if (modeRoles[j] !== specialRole) {
-								if (currentPlayer.roles.cache.some(role => role.name === modeRoles[j]))
-									currentPlayer.roles.remove(currentPlayer.roles.cache.find(role => role.name.toLowerCase() === modeRoles[j].toLowerCase()))
-							}
-						}
-						currentPlayer.roles.add(serverRole.id)
-					}
-				}
-			}
-			if (mentionPlayers !== '')
-				msg.channel.send(mentionPlayers)
+		if (!msg.content.startsWith("!rt") && !msg.content.startsWith("!ct")) return
+		const commandParams = msg.content.split(/\s+/)
+		if (commandParams[1] === undefined || commandParams[1] === "") {
+			msg.author.send("Error. Specify a player/event")
 			return msg.delete()
 		}
+		const modes = commandParams[0].split("!")
+		const globalMode = modes[1]
+
+		//console.log(msg.guild.members)
+		//...
+		if (!msg.member.hasPermission("MANAGE_ROLES")) return msg.reply("This command is for legendary bosses of the almighty MMR system only")
+		const rtRoles = ["RT Bronze", "RT Silver I", "RT Silver II", "RT Gold I", "RT Gold II", "RT Platinum", "RT Diamond", "RT Master"]
+		const ctRoles = ["CT Bronze", "CT Silver I", "CT Silver II", "CT Gold", "CT Platinum", "CT Diamond", "CT Master"]
+		const modeRoles = (globalMode === "rt") ? rtRoles : ctRoles
+		const result = await getRequest(globalMode, commandParams[1])
+		if (!result) {
+			msg.author.send("Error. Unable to find player/event with the name/id " + commandParams[1])
+			return msg.delete()
+		}
+		var mentionPlayers = ''
+
+
+		if (isNaN(commandParams[1])) {
+			//extra logic for additional players
+			let currentPlayer = msg.guild.members.cache.find(member => member.displayName.toLowerCase().replace(/\s/g, '').replace("é", '') === result[0].toLowerCase().replace(/\s/g, '').replace("é", ''))
+			if (currentPlayer === undefined) {
+				msg.author.send("Unable to find server member with the name " + commandParams[1])
+				return msg.delete()
+			}
+			for (j = 0; j < modeRoles.length; j++) {
+				currentPlayer = msg.guild.members.cache.find(member => member.displayName.toLowerCase().replace(/\s/g, '').replace("é", '') === result[0].toLowerCase().replace(/\s/g, '').replace("é", '') && member.roles.cache.some(role => role.name === modeRoles[j]))
+				if (currentPlayer !== undefined)
+					break
+			}
+			if (currentPlayer === undefined) {
+				msg.author.send(commandParams[1] + " does not have a rank role yet.")
+				return msg.delete()
+			}
+			//end
+			let serverRole = msg.guild.roles.cache.find(role => role.name === result[1])
+			if (!currentPlayer.roles.cache.some(role => role.name === serverRole.name)) {
+				for (j = 0; j < modeRoles.length; j++) {
+					if (currentPlayer.roles.cache.some(role => role.name === modeRoles[j]))
+						currentPlayer.roles.remove(currentPlayer.roles.cache.find(role => role.name.toLowerCase() === modeRoles[j].toLowerCase()))
+				}
+				let fromPenText = (commandParams[2] === "np") ? "" : "(from pen)"
+				currentPlayer.roles.add(serverRole.id)
+				mentionPlayers += `${currentPlayer} :` + result[1].replace(globalMode.toUpperCase() + " ", '').toLowerCase().replace(" ii", ': II').replace(" i", ': I').replace("platinum", "plat")
+				mentionPlayers += (mentionPlayers[mentionPlayers.length-1] === "I") ? ` ${fromPenText}\n` : `: ${fromPenText}\n`
+			}
+		} else {
+			if (result !== ',') {
+				let resultarray = result.split(",")
+				const ranks = resultarray.slice(0, resultarray.length/2)
+				const players = resultarray.slice(resultarray.length/2, resultarray.length)
+				for (i = 0; i < players.length; i++) {
+					let currentPlayer = msg.guild.members.cache.find(member => member.displayName.toLowerCase().replace(/\s/g, '').replace("é", '') === players[i].toLowerCase().replace(/\s/g, '').replace("é", ''))
+					if (currentPlayer === undefined) {
+						msg.author.send("Unable to find server member with the name " + players[i]).catch((e) => console.log(e))
+						continue
+					}
+					//...
+					for (j = 0; j < modeRoles.length; j++) {
+						currentPlayer = msg.guild.members.cache.find(member => member.displayName.toLowerCase().replace(/\s/g, '').replace("é", '') === players[i].toLowerCase().replace(/\s/g, '').replace("é", '') && member.roles.cache.some(role => role.name === modeRoles[j]))
+						if (currentPlayer !== undefined)
+							break
+					}
+					if (currentPlayer === undefined) {
+						msg.author.send(players[i] + " does not have a rank role yet.").catch((e) => console.log(e))
+						continue
+					}
+					//...
+					mentionPlayers += `${currentPlayer} :` + ranks[i].replace(globalMode.toUpperCase() + " ", '').toLowerCase().replace(" ii", ': II').replace(" i", ': I').replace("platinum", "plat")
+					mentionPlayers += (mentionPlayers[mentionPlayers.length-1] === "I") ? "\n" : ":\n"
+					let serverRole = msg.guild.roles.cache.find(role => role.name === ranks[i])
+					const specialRole = modeRoles[modeRoles.indexOf(ranks[i])]
+					for (j = 0; j < modeRoles.length; j++) {
+						if (modeRoles[j] !== specialRole) {
+							if (currentPlayer.roles.cache.some(role => role.name === modeRoles[j]))
+								currentPlayer.roles.remove(currentPlayer.roles.cache.find(role => role.name.toLowerCase() === modeRoles[j].toLowerCase()))
+						}
+					}
+					currentPlayer.roles.add(serverRole.id)
+				}
+			}
+		}
+		if (mentionPlayers !== '')
+			msg.channel.send(mentionPlayers)
+		return msg.delete()
 	} catch (error) {
 		console.error('ERROR:')
         console.error(error)
