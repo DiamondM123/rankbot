e7bc0b43b18ccc66a16c6235cf1eaef77484d12e
DiamondM123 <63083781+DiamondM123@users.noreply.github.com>
2021-01-20 17:04:11 -0600

    Update index.js

--- index.js
+++ index.js
@@ -66,7 +66,7 @@ const getRequest = async (mode, warid, msg_obj) => {
 						if (!currentPlayer.roles.cache.some(role => role.id == (mode == "rt" ? '800958350446690304' : '800958359569694741'))) {
 							msg_obj.channel.send(`<@${currentPlayer.id}> ` + emoji('top', msg_obj));
 						}
-						currentPlayer.roles.add(mode == "rt" ? '800958350446690304' : '800958359569694741');
+						await currentPlayer.roles.add(mode == "rt" ? '800958350446690304' : '800958359569694741');
 					} else if (currentPlayer != undefined) {
 						//console.log("not good");
 						await currentPlayer.roles.remove(mode == "rt" ? '800958350446690304' : '800958359569694741');
