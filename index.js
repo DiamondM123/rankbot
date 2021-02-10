8cae69667bb44de7c5021af9acaa0df09eef028e
DiamondM123 <63083781+DiamondM123@users.noreply.github.com>
2021-02-10 00:02:14 -0600

    Update index.js

--- index.js
+++ index.js
@@ -274,6 +274,9 @@ client.on('message', async msg => {
 		}
 
 		msg.content = msg.content.toLowerCase();
+		// let hahaha = await downloadPage("https://mariokartboards.com/lounge/json/player.php?type=rt&name=Fox,kenchan,Killua,neuro,Shaun,Jeff,Kaspie,barney,meraki,pachu,Quinn,Leops,Mikey,jun,Sane,rusoX,Az,EmilP,Batcake,Taz,Sora,Dane,lo,Solar,Goober");
+		// hahaha = JSON.parse(hahaha);
+		// console.log(hahaha);
 		if (!msg.content.startsWith("!rt") && !msg.content.startsWith("!ct") && !msg.content.startsWith("!dp") && !msg.content.startsWith("!top50")) return;
 		const rolesThatCanUpdate = ['387347888935534593', '792805904047276032', '399382503825211393', '399384750923579392', '521149807994208295', '792891432301625364', '521154917675827221', '393600567781621761', '520808645252874240'];
 		// 504795505583456257
